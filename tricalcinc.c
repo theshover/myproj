61615704dfcfaebf0c132c275fc742c606965336
theshover <jscaptcrazy2@gmail.com>
2016-08-12 14:33:21 -0400

    Create tricalcinc.c

---
+++ tricalcinc.c
@@ -0,0 +1,37 @@
+#include <stdio.h> // may need to add math.h
+#include <math.h> // so i did 
+
+int main ()
+{
+float A , B , C;
+float abtestone;
+	// will need to add more variables later
+printf ("Tricalc V 0.0.01" /n);
+printf ("Written by James Shover" /n);
+printf ("If you have any thing to tell me, message me through email at jamesshoverdev@gmail.com" /n /n /n);
+	
+	
+printf	("Press the number for side A if this is all unknown put 'd'." /n);
+scanf ( const float * A ); // prob gonna need to use differ func but for now i hope it will work
+printf ( A /n );
+
+printf	("Press the number for side B if this is all unknown put 'd'." /n);
+scanf ( const float * B );
+printf ( B /n );	
+
+printf	("Press the number for side C if this is all unknown put 'd'.");
+scanf ( const float * C );
+printf ( C /n);
+
+/*
+collecting data to be processed
+*/
+	
+if (A == "d") {} // gotta come back to this
+// gonna have to use a struct i think add a pointer idk
+	
+((A += A * A) (B += B * B) == (C += C * C ) );
+	
+if (  ) {}
+	
+}
