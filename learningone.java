b2c318c79d5d0476ff5b0e923d91ff09dcc0c56d
theshover <jscaptcrazy2@gmail.com>
2016-08-12 14:31:20 -0400

    Create learningone.java

---
+++ learningone.java
@@ -0,0 +1,8 @@
+public class learningone
+	
+public static void int main (char testvar = "Hello User")
+	{
+		System.out.println (testvar);
+	}
+// test of using variables to hold a string and then print the string through the variable
+// end java
